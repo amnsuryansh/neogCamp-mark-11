e5ee02681130ab19b44cabf1deddade27ff78c7a
Shivansh Shukla <princesuryansh2000@gmail.com>
2021-09-25 12:52:33 +0530

    feat:birthday lucky done

---
+++ app.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum ,luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Birthday is lucky";
+    } else {
+        outputBox.innerText = "Your birthday is not lucky";
+    }
+}
+
+function checkNumberIsLucky() {
+    const bdate = dateOfBirth.value;
+    const sum = calculateSum(bdate);
+    if (sum && bdate) {
+        compareValues(sum, luckyNumber);
+    }
+    else {
+        outputBox.innerText = "please enter both the fields";
+    }
+}
+
+function calculateSum(bdate) {
+    bdate = bdate.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < bdate.length; i++) {
+        sum = sum + Number(bdate.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click",
+    checkNumberIsLucky);
+
